075c1036598d884396cc1df9d7ca6c8be122d6ff
Patrick Wieth <patrick.wieth@googlemail.com>
2020-02-02 19:21:14 +0100

    implement card gallery

--- src/components/pages/GalleryPage.vue
+++ src/components/pages/GalleryPage.vue
@@ -1,59 +1,78 @@
 <template>
+  <div>
     <div class="gallery-view">
-      <CardComponent v-bind:model="sampleCard" v-bind:active-step="3"></CardComponent>
-      <CardComponent v-bind:model="sampleCard" v-bind:active-step="3"></CardComponent>
-      <CardComponent v-bind:model="sampleCard" v-bind:active-step="3"></CardComponent>
-      <CardComponent v-bind:model="sampleCard" v-bind:active-step="3"></CardComponent>
-      <CardComponent v-bind:model="sampleCard" v-bind:active-step="3"></CardComponent>
-      <CardComponent v-bind:model="sampleCard" v-bind:active-step="3"></CardComponent>
+      <div v-for="(card, index) in cards" v-bind:key="index">
+        <CardComponent v-bind:model="card" v-bind:imageURL="sampleImage" v-bind:id="'card'+index"></CardComponent>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-// import axios from 'axios'
+import * as R from 'ramda'
 import ContentContainerComponent from '@/components/ContentContainerComponent'
 import CardComponent from '@/components/CardComponent'
+import { sampleImg } from '../utils.js'
 
 export default {
   name: 'GalleryPage',
   components: {CardComponent, ContentContainerComponent},
   data () {
     return {
-      cards: null,
-      sampleCard: {
-        name: 'Name',
-        description: '',
-        ability: '',
-        notes: '',
-        article: 'the',
-        surname: 'Surname',
-        type: 'No Type',
-        tags: [],
-        cost: {
-          lumber: 0,
-          food: 0,
-          iron: 0,
-          mana: 0,
-          energy: 0,
-          generic: 0
-        },
-        ticks: 0,
-        defense: 0,
-        attack: 0
-      }
+      cards: [],
+      cardImgs: [],
+      sampleImage: sampleImg
     }
   },
   mounted () {
-    this.$http.get('cardservice/cards ')
+    let that = this
+
+    this.$http.get('cardservice/cards')
       .then(res => {
-        console.log(res)
+        // console.log(res)
 
-        let cardData = res.data.data
+        let relevantCards = R.filter(item => item.Content, R.map(item => JSON.parse(item), res.data))
 
-        cardData.forEach(function (card) {
-          this.cards.add('ELEMENT')
+        let contentLens = R.lensProp('Content')
+        let parseContent = item => R.set(contentLens, JSON.parse(atob(item.Content)), item)
+        relevantCards = R.map(parseContent, relevantCards)
+
+        relevantCards.forEach(function (card) {
+          let cardType = R.keys(card.Content)
+          console.log(cardType[0])
+          if (cardType) {
+            card = R.merge(card, card.Content[cardType[0]])
+            console.log(card)
+          }
+          console.log(card)
+
+          let parsedCard = {
+            'name': card.Name,
+            'type': cardType[0],
+            'health': card.Health || 0,
+            'attack': card.Attack || 0,
+            'speed': card.CastSpeed,
+            'cost': {
+              'lumber': 2,
+              'iron': 3,
+              'mana': 1,
+              'energy': 1,
+              'food': 1,
+              'generic': 1
+            },
+            'abilities': [],
+            'tag': card.Tag,
+            'text': card.Text,
+            'image': ''
+          }
+          that.cards.push(parsedCard)
         })
       })
+  },
+  methods: {
+    getSampleImg () {
+      return sampleImg
+    }
   }
 }
 </script>
