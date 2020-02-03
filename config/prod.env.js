ed16e79468df957f05211c41cf1d0d9c21623049
Patrick Wieth <patrick.wieth@googlemail.com>
2020-02-03 19:48:07 +0100

    change blockchain endpoint to https

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  VUE_APP_BLOCKCHAIN_API: '"http://v220190910354396996.luckysrv.de"',
+  VUE_APP_BLOCKCHAIN_API: '"https://cardchain.crowdcontrol.network"',
   VUE_APP_CHAIN_ID: '"testCardchain"',
   VUE_APP_CREATOR_ADDRESS: '"cosmos178x4cwg7zuppfgypdd7c0wy0kp304wad9v0awe"',
   VUE_APP_CREATOR_MNEMONIC: '"obey regular kiwi crop middle letter ten ivory foot supreme spice job beef dignity loan easy amateur wall skill cube random post grunt blind"'
