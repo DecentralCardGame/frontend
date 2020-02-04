acbfa99fe9f3ae828769e57883bbbbdfb02158d9
patrickwieth <patrick.wieth@googlemail.com>
2020-02-04 13:19:13 +0100

    fix AboutPage

--- src/components/pages/AboutPage.vue
+++ src/components/pages/AboutPage.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="hero">
-    Crowd Control is a trading card game, which does not only give you full ownership of your cards but also give the community full ownership of the game. <br>
-    This means we are creating this together. Every player can design his own trading cards. Every player may participate in the process of balancing the game by voting on cards. <br>
+    Crowd Control is a trading card game, which gives you full ownership of your cards AND gives the community full ownership of the game. <br><br>
+    This means we are creating this together. Every player can design his own trading cards. Every player may participate in the process of balancing the game by voting on cards. <br><br>
     This sounds very awesome, but how shall this work? Somebody must be in charge and someone has to say what is overpowered and why should anyone do all this kind of work? <br>
     Yes. The answer is blockchain. We build a blockchain with Cosmos technology that incentivizes all of these processes and disincentivizes trolling. <br>
-    How all of this works can be read on our project page <a href="https://www.decentral-card.net/" Decentral Card Network></a><br>
-    You are right now on the page of the game Crowd Control, which is the product of our decentralized autonomous organization (DAO)
+    How all of this works can be read on our project page <a href="https://www.decentral-card.net/" target="_blank"> Decentral Card Network </a><br><br>
+    You are right now on the page of the game Crowd Control, which is the product of our decentralized autonomous organization (DAO).
   </div>
 </template>
 
@@ -20,6 +20,6 @@ export default {
 <style scoped>
 .hero {
   margin: 4em 0;
-  font-size: 48px;
+  font-size: 24px;
 }
 </style>
