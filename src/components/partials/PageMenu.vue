f785bc17543d8238fef43d7324b304916e2186b6
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2020-09-09 17:05:36 +0200

    menu bar animation fix

--- src/components/partials/PageMenu.vue
+++ src/components/partials/PageMenu.vue
@@ -164,22 +164,93 @@ export default {
       width: 100%;
     }
 
+    /*
+    .hvr-bounce-to-right {
+  display: inline-block;
+  vertical-align: middle;
+  -webkit-transform: perspective(1px) translateZ(0);
+  transform: perspective(1px) translateZ(0);
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+  position: relative;
+  -webkit-transition-property: color;
+  transition-property: color;
+  -webkit-transition-duration: 0.5s;
+  transition-duration: 0.5s;
+}
+.hvr-bounce-to-right:before {
+  content: "";
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #2098D1;
+  -webkit-transform: scaleX(0);
+  transform: scaleX(0);
+  -webkit-transform-origin: 0 50%;
+  transform-origin: 0 50%;
+  -webkit-transition-property: transform;
+  transition-property: transform;
+  -webkit-transition-duration: 0.5s;
+  transition-duration: 0.5s;
+  -webkit-transition-timing-function: ease-out;
+  transition-timing-function: ease-out;
+}
+.hvr-bounce-to-right:hover, .hvr-bounce-to-right:focus, .hvr-bounce-to-right:active {
+  color: white;
+}
+.hvr-bounce-to-right:hover:before, .hvr-bounce-to-right:focus:before, .hvr-bounce-to-right:active:before {
+  -webkit-transform: scaleX(1);
+  transform: scaleX(1);
+  -webkit-transition-timing-function: cubic-bezier(0.52, 1.64, 0.37, 0.66);
+  transition-timing-function: cubic-bezier(0.52, 1.64, 0.37, 0.66);
+}
+     */
+
     a, .menu-item {
       padding: 1.5rem;
-      display: block;
       color: $white;
       text-align: center;
-      background: linear-gradient(to right, $white 50%, $blue 50%);
-      background-size: 200% 100%;
-      background-position: right bottom;
-      transition: all $animation-duration ease-out;
       cursor: pointer;
       text-decoration: none;
-      &:hover, &.activated {
-        transition: all 0.1s;
-        border-color: $white;
-        background-position: left bottom;
+
+
+      display: inline-block;
+      vertical-align: middle;
+      -webkit-transform: perspective(1px) translateZ(0);
+      transform: perspective(1px) translateZ(0);
+      position: relative;
+      -webkit-transition-property: color;
+      transition-property: color;
+      -webkit-transition-duration: 0.5s;
+      transition-duration: 0.5s;
+      &:before {
+        content: "";
+        position: absolute;
+        z-index: -1;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: $white;
+        -webkit-transform: scaleX(0);
+        transform: scaleX(0);
+        -webkit-transform-origin: 0 50%;
+        transform-origin: 0 50%;
+        -webkit-transition-property: transform;
+        transition-property: transform;
+        -webkit-transition-duration: $animation-duration;
+        transition-duration: $animation-duration;
+        -webkit-transition-timing-function: ease-out;
+        transition-timing-function: ease-out;
+      }
+      &:hover, &:focus, &:active {
         color: $red;
+      }
+      &:hover:before, &:focus:before, &:active:before {
+        -webkit-transform: scaleX(1);
+        transform: scaleX(1);
       }
     }
     &.exposed {
