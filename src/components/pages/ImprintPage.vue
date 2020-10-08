dd63a66b84651a05d370a85c85d6fdc74d674ad6
Patrick Wieth <patrick.wieth@googlemail.com>
2020-10-08 15:46:42 +0200

    add imprintpage

---
+++ src/components/pages/ImprintPage.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    Angaben gemäß § 5 TMG:
+    Patrick Wieth
+    Hügelstraße 85a
+    64283 Darmstadt
+    Telefon: +49 6151 3914 603
+    E-Mail: patrick.wieth@googlemail.de
+
+
+
+
+    Haftungsausschluss (Disclaimer)
+    Haftung für Inhalte
+    Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+
+    Haftung für Links
+    Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+
+    Urheberrecht
+    Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+
+
+    Privacy Policy
+    Deine Privats- und Persoenlichkeitssphaere ist uns wichtig. Wir beachten daher bei der Verarbeitung Deiner personenbezogenen Daten streng die gesetzlichen Bestimmungen. Mit der Registrierung zum Abschluss eines Nutzungsvertrages bzw. der Nutzung unseres Angebotes stimmst Du nachfolgenden Datenschutzbedingungen zu:
+
+    Erhebung, Verarbeitung und Nutzung von personenbezogenen Daten Personenbezogene Daten sind nach § 3 Abs. 1 BDSG Angaben ueber sachliche und persoenliche Verhaeltnisse einer bestimmten oder bestimmbaren natuerlichen Person. Das sind beispielweise Dein Name, Deine Anschrift und Deine E-Mailadresse. Um unser Angebot nutzen zu koennen ist die Anlage eines Benutzerkontos noetig. Hierfuer benoetigen wir bei der Anmeldung folgende Daten von Dir: Deine E-Mailadresse und ein beliebiges Passwort. Freiwillige Profilangaben sind weiterhin ein beliebiger Benutzername sowie dein Vor- und Nachname wie ein beliebiges Profilfoto. Wir geben Deine vorgenannten Daten selbstverstaendlich nicht an Dritte weiter. Du erhaeltst nach erfolgter Anmeldung eine E-Mail mit einem Link zur Bestaetigung der Anmeldung fuer Dein Benutzerkonto. Eine Loeschung Deiner vorgenannten Daten bei uns erfolgt, sofern gesetzliche Aufbewahrungspflichten dem nicht entgegenstehen und wenn Du einen Loeschungsanspruch geltend gemacht hast, wenn die Daten zur Erfuellung des mit der Speicherung verfolgten Zwecks nicht mehr erforderlich sind oder wenn ihre Speicherung aus sonstigen gesetzlichen Gruenden unzulaessig ist. Deine Bewertungen und Kommentare bleiben aus Gruenden der Funktionsfaehigkeit unseres Angebotes bestehen, werden jedoch anonymisiert angezeigt.
+    Funktionen in unserem Angebot, Standortfreigabe Unser Angebot erstellt Dir nach Eingabe deines Start- und Zielortes sowie optional von Vorlieben und Wuenschen eine Reiseroute. Du erhaeltst auch uebernachtungsvorschlaege auf der Route. Sofern du Deinen Standort in unserer App freigegeben hast, wird dieser an uns zur Ermittlung von Routenvorschlaegen uebermittelt. Eine weitergehende Speicherung erfolgt nicht. Du kannst einzelnen Stationen Deiner Reiseroute und allgemein Routenpunkte anhand einer Skala bewerten. Die Bewertungen aller Benutzer werden zu einem Durchschnitt berechnet, der fuer alle anderen Nutzer unseres Angebotes einsehbar ist. Daneben koennen Routenpunkte auch kommentiert werden. Zudem kannst Du selbst Routenpunkte (sog. Points of Interest, POI) anlegen und Bilder hierfuer hochladen. Die Herkunft der Daten und Bilder ist fuer andere Nutzer jedoch nicht ersichtlich; eine Verlinkung mit Deinem Benutzerprofil erfolgt nur intern um Missbrauch zu verhindern und ist fuer andere Nutzer nicht sichtbar. Sofern Du eine Reiseroute mit jemand anderem teilst, wird fuer den Empfaenger der Reiseroute Dein Benutzername sichtbar; sofern du keinen Benutzername angibst, wird ein generischer Benutzername erstellt, den du jederzeit aendern kannst.
+    Verwendung von Cookies Auf verschiedenen Seiten unserer Website werden sogenannte “Cookies” verwendet, welche dazu dienen, unseren Internetauftritt insgesamt nutzerfreundlicher, effektiver sowie sicherer zu machen – etwa wenn es darum geht, das Navigieren auf unserer Website zu beschleunigen. Zudem versetzen uns Cookies in die Lage, etwa die Haeufigkeit von Seitenaufrufen und die allgemeine Navigation zu messen. Bei Cookies handelt es sich um kleine Textdateien, die auf Deinem Computersystem abgelegt werden. Wir weisen Dich darauf hin, dass einige dieser Cookies von unserem Server auf Deine, Computersystem ueberspielt werden, wobei es sich dabei meist um so genannte “Session–Cookies” handelt. “Session-Cookies” zeichnen sich dadurch aus, dass diese automatisch nach Ende der Browser-Sitzung wieder von Deiner Festplatte geloescht werden. Andere Cookies verbleiben auf Deinem Computersystem und ermoeglichen es uns, Dein Computersystem bei Deinem naechsten Besuch wieder zu erkennen (sog. dauerhafte Cookies). Die meisten Webbrowser besitzen in ihrer Menueleiste die Hilfe-Funktion, die Dir erklaert, wie Du Deinen Browser so einstellst, dass neue Cookies akzeptiert werden. Zusaetzlich kannst Du Deinen Browser so einstellen, dass er Dich benachrichtigt, wenn Du neue Cookies erhalten hast und wie Du saemtliche bereits erhaltene Cookies loeschen oder fuer alle weiteren sperren kannst. Bitte beachte jedoch, dass Du mit der Verhinderung der Installation von Cookies gegebenenfalls nicht saemtliche Funktionen dieser Webseite vollumfaenglich nutzen kannst.
+    Social Plugins
+    Facebook Auf unserer Website werden sogenannte Social Plugins (“Plugins”) des sozialen Netzwerkes Facebook, das von der Facebook Inc., 1601 S. California Ave, Palo Alto, CA 94304, USA (“Facebook”) betrieben wird, verwendet. Die Plugins sind mit einem Facebook-Logo oder dem Zusatz “Soziales Plug-in von Facebook” gekennzeichnet. Wenn Du eine Seite unseres Webauftritts aufrufst, die ein solches Plugin enthaelt, baut Dein Browser eine direkte Verbindung mit den Servern von Facebook auf. Der Inhalt des Plugins wird von Facebook direkt an Deinen Browser uebermittelt und von diesem in die Webseite eingebunden. Durch die Einbindung der Plugins erhaelt Facebook die Information, dass Dein Browser die entsprechende Seite unserer Website aufgerufen hat, auch wenn Du kein Facebook-Konto besitzst oder gerade nicht bei Facebook eingeloggt bist. Diese Information (einschließlich Deiner IP-Adresse) wird von Deinem Browser direkt an einen Server von Facebook in den USA uebermittelt und dort gespeichert. Bist Du bei Facebook eingeloggt, kann Facebook den Besuch unserer Website Deinem Facebook- Konto direkt zuordnen. Wenn Du mit den Plugins interagierst, zum Beispiel den “Gefaellt mir”-Button betaetigst, wird die entsprechende Information ebenfalls direkt an einen Server von Facebook uebermittelt und dort gespeichert. Die Informationen werden zudem auf Facebook veroeffentlicht und Deinen Facebook-Freunden angezeigt. Facebook kann diese Informationen zum Zwecke der Werbung, Marktforschung und bedarfsgerechten Gestaltung der Facebook-Seiten benutzen. Hierzu werden von Facebook Nutzungs- , Interessen- und Beziehungsprofile erstellt, z. B. um Deine Nutzung unserer Website im Hinblick auf die Dir bei Facebook eingeblendeten Werbeanzeigen auszuwerten, andere Facebook-Nutzer ueber Deine Aktivitaeten auf unserer Website zu informieren und um weitere mit der Nutzung von Facebook verbundene Dienstleistungen zu erbringen. Wenn Du nicht moechtest, dass Facebook die ueber unseren Webauftritt gesammelten Daten Deinem Facebook-Konto zuordnet, musst Du sich vor Deinem Besuch unserer Website bei Facebook aus- loggen. Zweck und Umfang der Datenerhebung und die weitere Verarbeitung und Nutzung der Daten durch Facebook sowie Deine diesbezueglichen Rechte und Einstellungsmoeglichkeiten zum Schutz Deinem Privatsphaere entnimmst Du bitte den Datenschutzhinweisen von Facebook.
+    Twitter Unsere Website verwendet weiterhin Funktionen des Dienstes Twitter. Diese werden angeboten durch die Twitter Inc., Twitter, Inc. 1355 Market St, Suite 900, San Francisco, CA 94103, USA. Wenn Du eine Seite aufrufst, die ein solches Plugin enthaelt, baut Dein Browser eine direkte Verbindung mit den Servern von Twitter auf. Durch das Benutzen von Twitter und der Funktion “Re- Tweet” werden die von Dir besuchten Webseiten mit Deinem Twitter-Account verknuepft und anderen Nutzern bekannt gegeben. Dabei werden auch Daten an Twitter uebertragen. Zweck und Umfang der Datenerhebung und die weitere Verarbeitung und Nutzung der Daten durch Twitter sowie Deine diesbezueglichen Rechte und Einstellungsmoeglichkeiten zum Schutz Deiner Privatssphaere entnimmst Du bitte den Datenschutzhinweisen von Twitter. Deine Datenschutzeinstellungen bei Twitter kannst Du in den Konto-Einstellungen unter http://twitter.com/account/settings aendern.
+    Google+ Unsere Website verwendet Funktionen (“Plugins”) von Google+, welches von der Google Inc., 1600 Amphitheatre Parkway, Mountain View, California 94043, USA betrieben wird. Wenn Du eine Seite aufrufst, die ein solches Plugin enthaelt, baut Dein Browser eine direkte Verbindung mit den Servern von Google auf. Der Inhalt des Plugins wird von Google direkt an Deinen Browser uebermittelt und von diesem in die Webseite eingebunden. Durch die Einbindung der Plugins erhaelt Google+ die Information, dass Du die entsprechende Seite aufgerufen hast. Bist Du bei Google+ eingeloggt kann Google+ den Besuch Deinem Google+ Konto zuordnen. Zweck und Umfang der Datenerhebung und die weitere Verarbeitung und Nutzung der Daten durch Google+ sowie Deine diesbezueglichen Rechte und Einstellungsmoeglichkeiten zum Schutz Deiner Privatssphaere kannst Du den Datenschutzhinweisen von Google+ entnehmen.
+    Foursquare Unsere Website verwendet Social Plugins (“Plugins”) von foursquare.com, die von Foursquare Labs Inc., 568 Broadway, 10th Floor, New York, NY 10012, USA betrieben werden. Wenn Du eine Seite aufrufst, die ein solches Plugin enthaelt, baut Dein Browser eine direkte Verbindung mit den Servern von Foursquare auf. Der Inhalt des Plugins wird von Foursquare direkt an Deinen Browser uebermittelt und von diesem in die Webseite eingebunden. Durch die Einbindung der Plugins erhaelt Foursquare die Information, dass Du die entsprechende Seite aufgerufen hast. Bist Du bei Foursquare eingeloggt kann Foursquare den Besuch Deinem Foursquare- Konto zuordnen. Zweck und Umfang der Datenerhebung und die weitere Verarbeitung und Nutzung der Daten durch Foursquare sowie Deine diesbezueglichen Rechte und Einstellungsmoeglichkeiten zum Schutz Deiner Privatssphaere kannst Du den Datenschutzhinweisen von Foursquare entnehmen.
+    Verwendung von Facebook Connect, Google Login, Twitter Login, Foursquare Login Du kannst unser Angebot auch mit Deinem Facebook-, Google- oder Twitter-Benutzerkonto verwenden, d. h. eine zusaetzliche Registrierung entfaellt. Hierzu verwenden wir Facebook Connect, Google+ und Twitter Login. Sobald Du einen dieser Dienste nutzt, erhaelt der Anbieter Zugriff auf bestimmte Deiner persoenlichen Daten, insbesondere zu Deiner verschluesselten E-Mail Adresse. Diese Daten speichert der Anbieter unter anderem auch in den USA. Wir erhalten umgekehrt u. U. Zugriff auf Deine oeffentlich zugaenglichen Profildaten; dies kannst Du vor der Verbindung mit dem Benutzerkonto beim Anbieter freigeben. Weitere Einzelheiten findest Du in der Datenschutzerklaerung von Facebook unter https://de-de.facebook.com/full_data_use_policy, von Google unter http://www.google.de/intl/de/policies/privacy/, von Twitter unter https://twitter.com/privacy?lang=de sowie von Foursquare unter https://de.foursquare.com/legal/privacy.
+    Datenerhebung durch Nutzung Google Analytics Unser Angebot benutzt Google Analytics, einen Webanalysedienst der Google Inc. (“Google”). Google Analytics verwendet sog. “Cookies”, d.h. Textdateien, die auf dem Computer des Nutzers gespeichert werden und die eine Analyse der Benutzung der Website ermoeglichen. Die durch den Cookie erzeugten Informationen ueber die Benutzung dieser Website werden in der Regel an einen Server von Google in den USA uebertragen und dort gespeichert. Im Falle der Aktivierung der IP- Anonymisierung auf dieser Webseite, wird die IP-Adresse von Google jedoch innerhalb von Mitgliedstaaten der Europaeischen Union oder in anderen Vertragsstaaten des Abkommens ueber den Europaeischen Wirtschaftsraum zuvor gekuerzt. Nur in Ausnahmefaellen wird die volle IP- Adresse an einen Server von Google in den USA uebertragen und dort gekuerzt. Im Auftrag des Betreibers dieser Website wird Google diese Informationen benutzen, um die Nutzung der Website auszuwerten, um Reports ueber die Websiteaktivitaeten zusammenzustellen und um weitere, mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen gegenueber dem Websitebetreiber zu erbringen. Die im Rahmen von Google Analytics von dem Browser uebermittelte IP-Adresse wird nicht mit anderen Daten von Google zusammengefuehrt. Die Speicherung der Cookies kann durch eine entsprechende Einstellung der Browser-Software verhindert werden. Wir weisen jedoch darauf hin, dass in diesem Fall gegebenenfalls nicht saemtliche Funktionen dieser Website in vollem Umfang genutzt werden koennen. Darueber hinaus koennen die Erfassung der durch das Cookie erzeugten und auf die Nutzung der Website bezogenen Daten (inkl. IP-Adresse) an Google sowie die Verarbeitung dieser Daten durch Google verhindert werden, indem ein entsprechendes Browser-Plugin heruntergeladen und installiert wird. Wir weisen darauf hin, dass auf dieser Webseite Google Analytics, um den Code “gat._anonymizeIp();” erweitert wurde, um eine anonymisierte Erfassung von IP-Adressen (sog. IP- Masking) zu gewaehrleisten.
+    App-Berechtigungen Die App fordert vor der Installation Berechtigungen von Ihnen, die wir im Folgenden naeher ausfuehren: Internetzugriff Dies ermoeglicht der App, Dir Routenvorschlaege anzuzeigen. Ohne Internetzugriff kann unser Dienst nicht genutzt werden, da unsere Datenbanken fuer bestmoegliche Aktualitaet laufend auf unseren Servern aktualisiert werden. Zugriff auf GPS Dient dazu, um den Standort zu ermitteln und hierfuer passende Routenvorschlaege zu erstellen. Kamera verwenden Ermoeglicht es dir, Fotos von Orten und POIs zu schießen, die dann deinem Trip-Album hinzugefuegt und/oder anderen Nutzern bereitgestellt werden koennen. Hierdurch erhalten die Nutzer die Moeglichkeit, Vorschauen von POIs zu sehen.
+    Auskunftsrechte fuer den Datenschutz Nach dem Bundesdatenschutzgesetz hast Du ein Recht auf unentgeltliche Auskunft ueber Deine gespeicherten Daten sowie gegebenenfalls ein Recht auf Berichtigung, Sperrung, Loeschung bzw. Widerruf der Einwilligung zur zukuenftigen Verwendung dieser Daten. Bei Fragen zur Erhebung, Verarbeitung oder Nutzung Deiner personenbezogenen Daten, bei Auskuenften, Berichtigung, Sperrung oder Loeschung von Daten sowie Widerruf etwaiger erteilter Einwilligungen wende Dich bitte per E-Mail oder Brief an: gfnork UG (haftungsbeschraenkt) Eschollbruecker Straße 28A 64295 Darmstadt E-Mail kontakt@gfnork.de
+    Aenderung der Datenschutzerklaerung Wir behalten uns vor, diese Datenschutzerklaerung kuenftig zu aendern. Du solltest daher bei der Nutzung unseres Angebotes regelmaeßig die Datenschutzerklaerung einsehen.
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TeamPage',
+  components: {},
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import "src/assets/styles/variables";
+
+.hero {
+  margin: 4rem 0;
+
+  @media (max-width: 480px) {
+    margin: 0;
+  }
+}
+.image-cropper {
+  width: 200px;
+  height: 200px;
+  position: relative;
+  overflow: hidden;
+  border-radius: 50%;
+}
+.team-left {
+    display: grid;
+    grid-template-columns: 1fr 4fr;
+    grid-template-rows: 1fr;
+    gap: 2rem 4rem;
+    @media (min-width: 480px) {
+      grid-template-areas: "creator-input creator-preview";
+    }
+}
+img {
+  display: inline;
+  margin: 0 auto;
+  height: 100%;
+  width: auto;
+  float:left;
+}
+</style>
