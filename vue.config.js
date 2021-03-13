c50aae3bff1830fb7f84ad8a4fad6e51831a6e57
Patrick <patrick.wieth@googlemail.com>
2021-03-13 17:21:23 +0100

    adjust config to allow faster pre-loading

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,21 @@ module.exports = {
     watchOptions: {
       poll: true
     }
+  },
+  chainWebpack: (config) => {
+    /* 
+       Disable (or customize) prefetch, see:
+       https://cli.vuejs.org/guide/html-and-static-assets.html#prefetch
+    */
+    config.plugins.delete('prefetch')
+
+    /* 
+       Configure preload to load all chunks
+       NOTE: use `allChunks` instead of `all` (deprecated)
+    */
+    config.plugin('preload').tap((options) => {
+      options[0].include = 'allChunks'
+      return options
+    })
   }
 };
