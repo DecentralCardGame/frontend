77d02a8e1e68fc12f9deb89cf76be58e403ccfa5
Patrick <patrick.wieth@googlemail.com>
2021-03-31 20:09:46 +0200

    more landingpage

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -8,7 +8,7 @@
         A Trading Card Game YOU OWN
         </center>
       </b>
-      <div class= "img--container--top">
+      <div class= "img--container adjust--top">
         <img src="../assets/animations/sparkling_water.gif" class="image2" alt="yes...">
       </div>
     </div>
@@ -18,7 +18,7 @@
 
         <div class="hero--with--img">
 
-          <div class= "img--container">
+          <div class= "img--container adjust--pentagon">
             <svg viewBox="0 0 985.399 969.501" class="image3" >
               <path id="Pfad_298" data-name="Pfad 298" d="M132.32,2293.693l38.946,842.479,623.139-39.166,224.141-431.792-144.9-397.009Z" transform="translate(250.87 -2267.424) rotate(7)" fill="#4b0439"/>
               <g transform="translate(16,-12)">
@@ -56,7 +56,9 @@
 
       </div>
       <div class="dual--column--img">
-        <img src="mother_popup.gif" class="image3" alt="yes...">
+        <div class= "img--container adjust--mother--gif">
+          <img src="mother_popup.gif" class="image3" alt="yes...">
+        </div>
       </div>
     </div>
 
@@ -70,7 +72,7 @@
       <div class="dual--column--text">
 
         <div class="hero--with--img">
-          <div class= "img--container--trapez">
+          <div class= "img--container adjust--trapez">
             <svg viewBox="0 0 859.168 1347.934" class="image3" >
               <path id="Pfad_297" data-name="Pfad 297" d="M932.947,5899.424,872.038,4551.49l859.168,133.93-57.222,1144.968Z" transform="translate(-872.038 -4551.49)" fill="#4b0439"/>
               <g transform="translate(-10,-10)">
@@ -86,14 +88,16 @@
             </b>
             <p class="text--medium">
               Check out the cards created by fellow <br>
-              trading card gamers in the <router-link :to="{ name: 'Gallery' }">Gallery</router-link>. <br>
+              trading card gamers in the <router-link :to="{ name: 'Gallery' }"><b>Gallery</b></router-link>. <br>
             </p>
-            <div class=link--button>
-              <router-link to="/about" style="text-decoration: none; color: inherit;">
-                <p class=link--text>
-                  Go to Gallery >
-                </p>
-              </router-link>
+            <div class="button--container">
+              <div class=link--button>
+                <router-link to="/about" style="text-decoration: none; color: inherit;">
+                  <p class=text--button>
+                    Go to Gallery >
+                  </p>
+                </router-link>
+              </div>
             </div>
           </div>
 
@@ -103,14 +107,16 @@
             </b>
             <p class="text--medium">
               Make your own cards on the blockchain <br>
-              with our <router-link :to="{ name: 'New Card' }">Card Creator</router-link>.
+              with our <router-link :to="{ name: 'New Card' }"><b>Card Creator</b></router-link>.
             </p>
-            <div class=link--button>
-              <router-link to="/about" style="text-decoration: none; color: inherit;">
-                <p class=link--text>
-                  Go to Card Creator >
-                </p>
-              </router-link>
+            <div class="button--container">
+              <div class=link--button>
+                <router-link to="/about" style="text-decoration: none; color: inherit;">
+                  <p class=text--button>
+                    Go to Card Creator >
+                  </p>
+                </router-link>
+              </div>
             </div>
           </div>
 
@@ -121,21 +127,23 @@
             <p class="text--medium">
               Crowd Control has unique game mechanics <br>
               and a new kind of gameplay. Read more <br>
-              about the game rules on <router-link :to="{ name: 'About' }">the Game</router-link> page.
+              about the game rules on 
+              <router-link :to="{ name: 'About' }"><b>the Game</b></router-link> page.
             </p>
-            <div class=link--button>
-              <router-link to="/about" style="text-decoration: none; color: inherit;">
-                <p class=link--text>
-                  Find out more >
-                </p>
-              </router-link>
+            <div class="button--container">
+              <div class=link--button>
+                <router-link to="/about" style="text-decoration: none; color: inherit;">
+                  <p class=text--button>
+                    Find out more >
+                  </p>
+                </router-link>
+              </div>
             </div>
           </div>
 
         </div>
       </div>
     </div>
-
 
 
     <div class="hero">
@@ -147,10 +155,27 @@
         with a blockchain built on Cosmos technology. Read our <a
             href="https://github.com/DecentralCardGame/whitepaper/blob/master/whitepaper.pdf"
             target="_blank"
-          > Whitepaper</a> 
+          ><b> Whitepaper</b></a> 
         to find out more.
       </p>
+
+      <div class="button--container extra--margin">
+        <div class=link--button>
+          <a
+              href="https://github.com/DecentralCardGame/whitepaper/blob/master/whitepaper.pdf"
+              target="_blank"
+              style="text-decoration: none; color: inherit;"
+          >   
+            <p class=text--button>
+              Go to Whitepaper >
+            </p> 
+          </a>
+
+        </div>
+      </div>
+
     </div>
+
   </div>
 </template>
 
@@ -194,7 +219,17 @@ h2 {
 }
 
 .info--item {
-  margin: 0rem 0rem;
+  margin: 0rem 0rem 3rem 0rem;
+}
+.button--container {
+  position: relative;
+  top: -0.5rem;
+  left: -4rem;
+  margin: 0rem;
+}
+.extra--margin {
+  margin: 0rem 14rem;
+  left: -18rem;
 }
 
 .text--huge {
@@ -220,10 +255,10 @@ h2 {
   font-size: $font-size-small;
   letter-spacing: 2;
 }
-.link--text {
+.text--button {
   font-family: $font-family-header; 
   font-weight: normal;
-  padding: 0.5rem 1rem;
+  padding: 0.25rem 1rem;
   font-size: $font-size-small;
   color: $black;
   text-align: center;
@@ -234,7 +269,7 @@ h2 {
   background-color: $main-color-c;
   box-shadow: $border-thickness-bold * 1.0 $border-thickness-bold * 1.0 0 $black;
   position: relative;
-  margin: 0.5rem 5rem;
+  margin: 0rem 5rem;
   transform: skewX(-25deg);
   font-size: $font-size-small;
   cursor: pointer;
@@ -267,17 +302,19 @@ h2 {
     transform: scaleX(1);
   }
   @media (max-width: 1178px) {
-    transform: skewX(0);
+    transform: skewX(-25deg);
     margin: 0;
   }  
 }
 
-.img--container--top {
+.img--container {
+  position: absolute;
   z-index: -1;
   pointer-events: none;
   display: flex;
   justify-content: center;
-  position: absolute;
+}
+.adjust--top {
   top: 0;
   left: 0;
   right: 0;
@@ -301,20 +338,13 @@ h2 {
     top: -8rem;
   }
 }
-
-.img--container {
-  z-index: -1;
-  pointer-events: none;
-  display: flex;
-  justify-content: center;
-  position: absolute;
-  left: -4.5rem;
+.adjust--mother--gif {
+  position: relative;
+  left: 0rem;
   right: 0;
+  height: 100%;
 
-  width: 480px;
-  height: 1000px;
-  
-  top: -16rem;
+  top: 10rem;
   @media (max-width: 1700px) {
   }
   @media (max-width: 1400px) {
@@ -332,12 +362,30 @@ h2 {
   }
 }
 
-.img--container--trapez {
-  z-index: -1;
-  pointer-events: none;
-  display: flex;
-  justify-content: center;
-  position: absolute;
+.adjust--pentagon {
+  left: -4.5rem;
+  right: 0;
+  width: 480px;
+  height: 1000px;
+
+  top: -16rem;
+  @media (max-width: 1700px) {
+  }
+  @media (max-width: 1400px) {
+  }
+  @media (max-width: 1000px) {
+    top: -11rem;
+    left: -1em;
+  }
+  @media (max-width: 800px) {
+    top: -8rem;
+    left: -5em;
+  }
+  @media (max-width: 480px) {
+    top: -8rem;
+  }
+}
+.adjust--trapez {
   left: -3rem;
   right: 0;
 
