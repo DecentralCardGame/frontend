9584a8e17ad29305c63e6b2cbb5955925e432edf
lxgr-linux <lxgr@protonmail.com>
2023-02-07 14:45:06 +0100

    #178 made useTx nicer

--- src/def-composables/useTx.ts
+++ src/def-composables/useTx.ts
@@ -1,48 +1,61 @@
 import { useClient } from "@/composables/useClient";
 import { useAddress } from "@/def-composables/useAddress";
-import type { DeliverTxResponse } from "@cosmjs/stargate/build/stargateclient"
+import type { StdFee } from "@cosmjs/launchpad";
+import type { DeliverTxResponse } from "@cosmjs/stargate/build/stargateclient";
 
-const FEE = {
+const FEE: StdFee = {
   amount: [{ amount: "0", denom: "stake" }],
   gas: "2000000000"
 };
 
-export const useTx: () => {
+const { address } = useAddress();
+
+class Content {
+  fee: StdFee;
+  memo: string;
+  value: any;
+
+  constructor(value: any = {}) {
+    this.value = Object.assign({
+      fromAddress: address.value,
+      creator: address.value
+    }, value);
+    this.fee = FEE;
+    this.memo = "";
+  }
+}
+
+export const useTxInstance: () => {
   registerForCouncil: () => Promise<DeliverTxResponse>;
   rewokeCouncilRegistration: () => Promise<DeliverTxResponse>;
   send: (coins: any[], to: string) => Promise<DeliverTxResponse>
 } = () => {
   const client = useClient();
-  const { address } = useAddress();
 
   const send = (coins: any[], to: string) => {
-    return client.CosmosBankV1Beta1.tx.sendMsgSend({
-      value: {
+    return client.CosmosBankV1Beta1.tx.sendMsgSend(
+      new Content({
         amount: coins,
-        fromAddress: address.value,
-        toAddress: to,
-      },
-      fee: FEE,
-    });
-  }
+        toAddress: to
+      }));
+  };
 
   const registerForCouncil = () => {
-    return client.DecentralCardGameCardchainCardchain.tx.sendMsgRegisterForCouncil({
-      value: {
-        creator: address.value,
-      },
-      fee: FEE,
-    });
-  }
+    return client.DecentralCardGameCardchainCardchain.tx.sendMsgRegisterForCouncil(new Content());
+  };
 
   const rewokeCouncilRegistration = () => {
-    return client.DecentralCardGameCardchainCardchain.tx.sendMsgRewokeCouncilRegistration({
-      value: {
-        creator: address.value,
-      },
-      fee: FEE,
-    });
-  }
+    return client.DecentralCardGameCardchainCardchain.tx.sendMsgRewokeCouncilRegistration(new Content());
+  };
 
   return { send, registerForCouncil, rewokeCouncilRegistration };
+};
+
+let instance: ReturnType<typeof useTxInstance>;
+
+export const useTx = () => {
+  if (!instance) {
+    instance = useTxInstance();
+  }
+  return instance;
 };
