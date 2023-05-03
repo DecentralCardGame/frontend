5a74d2536bf1bdb556418fbb1188ed3e3f332922
lxgr-linux <lxgr@protonmail.com>
2023-05-03 13:18:40 +0200

    #267 Fixed last input event 2

---
+++ src/def-composables/useLastInputEvent.ts
@@ -0,0 +1,25 @@
+import { readonly, ref, type Ref } from "vue";
+
+interface Event {
+  which: number
+}
+
+const useLastInputEventInstance = () => {
+  const state: Ref<Event> = ref({which: 0});
+
+  const set = (event: Event) => {
+    state.value = event
+    console.log(event)
+  }
+
+  return { lastInputEvent: readonly(state), setLastInputEvent: set };
+}
+
+let instance: ReturnType<typeof useLastInputEventInstance>;
+
+export const useLastInputEvent = () => {
+  if (!instance) {
+    instance = useLastInputEventInstance();
+  }
+  return instance;
+};
