cefeb685018cd6b44db8b60e6610007368961ed8
lxgr-linux <lxgr@protonmail.com>
2023-10-01 12:53:57 +0200

    Raname BuyCollection to BuyBoosterPack

--- src/def-composables/useTx.ts
+++ src/def-composables/useTx.ts
@@ -274,7 +274,7 @@ export const useTxInstance: () => {
       }),
     ].concat(
       [
-        "/DecentralCardGame.cardchain.cardchain.MsgBuyCollection",
+        "/DecentralCardGame.cardchain.cardchain.MsgBuyBoosterPack",
         "/DecentralCardGame.cardchain.cardchain.MsgOpenBoosterPack",
         "/DecentralCardGame.cardchain.cardchain.MsgVoteCard",
         "/DecentralCardGame.cardchain.cardchain.MsgConfirmMatch",
