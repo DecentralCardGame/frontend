23b8f1efa2e31f0e3185181acdb7f1eeee266857
lxgr-linux <lxgr@protonmail.com>
2023-12-14 16:16:02 +0100

    fixed transactions

--- src/views/CardCreatorPage.vue
+++ src/views/CardCreatorPage.vue
@@ -1083,7 +1083,7 @@ export default {
         )
         return
       }
-      if (this.designateArtist && !this.$cardChain.validAddress(this.artistAddress)) {
+      if (this.designateArtist && !validAddress(this.artistAddress)) {
         this.notifyFail(
           "Invalid Address",
           "The address given for designated artist is invalid."
