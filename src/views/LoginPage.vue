80d70d380038d0a4e9b1f8dbf403c24b0f13e2df
lxgr-linux <lxgr@protonmail.com>
2024-01-14 23:51:21 +0100

    made login page viewport relative and very nice

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,30 +1,34 @@
 <template>
-  <div class="bg-black flex flex-col">
-    <div class="my-[25rem] text-center">
-      <h2 class="text-white text-5xl font-bold p-7">
-        {{ isSignUpRequired ? "Sign up" : "Log in" }}
-      </h2>
-      <BaseCCButton v-if="!isSignUpRequired" @click="login"
-        >Login with keplr
-      </BaseCCButton>
-      <div v-else>
-        <div v-if="isKeplrAvailable" class="flex flex-col justify-center">
-          <p class="text-white p-7">You have to sign up first</p>
-          <vue-hcaptcha
-            :sitekey="state.siteKey"
-            @verify="onVerify"
-            class="mx-auto"
-          />
-        </div>
+  <div class="bg-black flex flex-col h-[80vh] sm:h-[85vh]">
+    <div class="text-center my-auto">
+      <div class="-mt-40">
+        <h2 class="text-white text-5xl font-bold p-7">
+          {{ isSignUpRequired ? "Sign up" : "Log in" }}
+        </h2>
+        <BaseCCButton v-if="!isSignUpRequired" @click="login"
+          >Login with keplr
+        </BaseCCButton>
         <div v-else>
-          <p class="text-white p-7">
-            To log in you first need download and install the
-            <a href="https://www.keplr.app" class="underline">keplr wallet</a>
-            browser extension
-          </p>
-          <a href="https://www.keplr.app/download" class="mx-auto">
-            <BaseCCButton :type="ButtonType.TEAL"> Download Keplr</BaseCCButton>
-          </a>
+          <div v-if="isKeplrAvailable" class="flex flex-col justify-center">
+            <p class="text-white p-7">You have to sign up first</p>
+            <vue-hcaptcha
+              :sitekey="state.siteKey"
+              @verify="onVerify"
+              class="mx-auto"
+            />
+          </div>
+          <div v-else>
+            <p class="text-white p-7">
+              To log in you first need download and install the
+              <a href="https://www.keplr.app" class="underline">keplr wallet</a>
+              browser extension
+            </p>
+            <a href="https://www.keplr.app/download" class="mx-auto">
+              <BaseCCButton :type="ButtonType.TEAL">
+                Download Keplr</BaseCCButton
+              >
+            </a>
+          </div>
         </div>
       </div>
     </div>
