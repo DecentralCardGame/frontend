dc317493cb7d1816e2ab13aab120fe22ae8be966
lxgr-linux <lxgr@protonmail.com>
2024-02-11 17:49:14 +0100

    #267 Updated Dropdown logic to be less awfull

--- src/components/elements/Dropdown/Dropdown.vue
+++ src/components/elements/Dropdown/Dropdown.vue
@@ -1,70 +1,65 @@
 <template>
   <div class="relative inline-block" @click="toggleDropdown">
     <!-- Trigger button -->
-    <button class="p-2 bg-[#8F6173] rounded-sm flex items-center shadow-xl"
-      :class="{'ring ring-white ring-opacity-100': isOpen}"
+    <button
+      class="p-2 bg-[#8F6173] rounded-sm flex items-center shadow-xl"
+      :class="{ 'ring ring-white ring-opacity-100': isOpen }"
     >
-      {{ display(selectedOption) }}
-      <img class="w-4 pr-1 rotate-180" src="@/assets/figma/Navigation_Triangle.svg" alt="navigation triangle" />
+      {{ displayButton() }}
+      <img
+        class="w-4 pr-1 rotate-180"
+        src="@/assets/figma/Navigation_Triangle.svg"
+        alt="navigation triangle"
+      />
     </button>
 
     <!-- Dropdown menu -->
-    <div v-if="isOpen"
+    <div
+      v-if="isOpen"
       class="absolute z-30 bg-[#8F6173] rounded-sm w-full text-center ring ring-white ring-opacity-100 hover:cursor-pointer"
     >
       <ul>
         <li
-          v-for="option in dropdownOptions" :key="getOptionKey(option)" @click="selectOption(option)"
+          v-for="(option, idx) in options"
+          :key="idx"
+          @click="selectOption(option)"
           class="hover:bg-white hover:bg-opacity-70 hover:text-[#D82027]"
         >
-          {{ display(option) }}
+          {{ displayFn(option) }}
         </li>
       </ul>
     </div>
-
-    
   </div>
 </template>
 
-<script setup lang="ts">
-import { ref, defineProps, defineEmits } from 'vue';
+<script setup lang="ts" generic="T">
+import { ref, defineProps, defineEmits, type Ref } from "vue";
 
-interface Option {
-  id: number;
-  label: string;
-}
-
-const emit = defineEmits(['update:modelValue'], ['change']);
-const props = defineProps(['options', 'initial', 'displayFn']); // Define a prop named 'options' that will be passed from the parent component
-
+const model = defineModel<T>();
 const isOpen = ref(false);
-const selectedOption = ref<string>(props.initial ? props.initial : '?');
 
-const dropdownOptions: (Option | number)[] = ref(props.options) || ref([]); // Use the prop or an empty array if not provided
+const props = withDefaults(
+  defineProps<{
+    options: Array<T>;
+    initial?: string;
+    displayFn?: (v: T) => string;
+  }>(),
+  {
+    options: () => [],
+    initial: "?",
+    displayFn: (v: T): string => "" + v,
+  }
+);
 
-const display = (option) => {
-  if (typeof props.displayFn == "function")
-    return typeof option === 'object' ? props.displayFn(option.label) : props.displayFn(option)
-  else
-    return typeof option === 'object' ? option.label : option
-}
+const displayButton = () => {
+  return model.value ? props.displayFn(model.value) : props.initial;
+};
 
 const toggleDropdown = () => {
   isOpen.value = !isOpen.value;
 };
 
-const getOptionKey = (option: Option | number) => {
-  return typeof option === 'object' ? option.id : option;
-};
-
-const selectOption = (option: Option | number) => {
-  if (typeof option === 'object') {
-    selectedOption.value = option.label;
-  } else {
-    selectedOption.value = option.toString();
-  }
-  // Emit the selected option to the parent component
-  emit('update:modelValue', option);
-  emit('change', option);
+const selectOption = (option: T) => {
+  model.value = option;
 };
 </script>
