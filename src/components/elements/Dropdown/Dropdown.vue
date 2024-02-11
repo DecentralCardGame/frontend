577be9bf94922c846520f4bb2275de8678fc7d18
lxgr-linux <lxgr@protonmail.com>
2024-02-11 20:28:55 +0100

    #267 Restyled dropdown

--- src/components/elements/Dropdown/Dropdown.vue
+++ src/components/elements/Dropdown/Dropdown.vue
@@ -1,39 +1,36 @@
 <template>
-  <div class="relative inline-block" @click="toggleDropdown">
-    <!-- Trigger button -->
-    <button
-      class="p-2 bg-[#8F6173] rounded-sm flex items-center shadow-xl"
-      :class="{ 'ring ring-white ring-opacity-100': isOpen }"
-    >
+  <div
+    class="relative inline-block bg-[#8F6173] text-left rounded-sm hover:cursor-pointer"
+    :class="{ 'ring ring-white ring-opacity-100': isOpen }"
+    @click="toggleDropdown"
+  >
+    <div class="p-2 flex">
       {{ displayButton() }}
       <img
-        class="w-4 pr-1 rotate-180"
+        class="w-4 pr-1 rotate-180 my-auto"
         src="@/assets/figma/Navigation_Triangle.svg"
         alt="navigation triangle"
       />
-    </button>
-
-    <!-- Dropdown menu -->
-    <div
-      v-if="isOpen"
-      class="absolute z-30 bg-[#8F6173] rounded-sm w-full text-center ring ring-white ring-opacity-100 hover:cursor-pointer"
-    >
-      <ul>
-        <li
-          v-for="(option, idx) in options"
-          :key="idx"
-          @click="selectOption(option)"
-          class="hover:bg-white hover:bg-opacity-70 hover:text-[#D82027]"
-        >
-          {{ displayFn(option) }}
-        </li>
-      </ul>
     </div>
+
+    <ul
+      v-if="isOpen"
+      class="absolute z-30 bg-[#8F6173] ring ring-white rounded-sm ring-opacity-100 whitespace-nowrap"
+    >
+      <li
+        v-for="(option, idx) in options"
+        :key="idx"
+        @click="selectOption(option)"
+        class="px-2 py-1 hover:bg-white hover:bg-opacity-70 hover:text-cc-red"
+      >
+        {{ displayFn(option) }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts" generic="T">
-import { ref, defineProps, defineEmits, type Ref } from "vue";
+import { ref, defineProps } from "vue";
 
 const model = defineModel<T>();
 const isOpen = ref(false);
