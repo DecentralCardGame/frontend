0505d7faa8b06220f3a915438c665e245bf6fae2
lxgr-linux <lxgr@protonmail.com>
2024-03-09 16:18:47 +0100

    feat(#275): Stupid

---
+++ src/views/Team/Community.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="flex justify-center text-center flex-col m-16 uppercase text-xl">
+    <img :src="logo" alt="logo" class="mx-auto w-72" />
+    <br />
+    <p>
+      As a DAO, we are encouraging decentralized contributions to the project.
+      Join the Project by contacting us on Discord.
+    </p>
+    <br />
+    <p class="font-bold">Our Community Contributors</p>
+  </div>
+</template>
+<script setup lang="ts">
+import logo from "@/assets/figma/CCLogo.png";
+
+const names = [
+  "RUSLAN",
+  "“Vnode_casH”",
+  "Carden",
+  "cryptobtcbuyer",
+  "MMS",
+  "BonyNode",
+  "safe",
+  "sminempepe#7667 (@sminempepe)",
+  "Gliderso",
+  "Tarzik",
+  "ArtiDS_Node",
+  "Cardep",
+  "stakr.space",
+  "Shoni",
+  "Yurbason",
+  "icodragon [NODERS]#4560",
+  "JahaND",
+  "gönlü zengin#8838",
+  "xAlex",
+  "Sr20de",
+  "Oxyde",
+  "Asadovka | Nodejumper",
+  "[NODERS]TEAM",
+  "molla202",
+  "dimasik",
+  "kjnodes",
+  "Cumulo",
+  "Nodes|Bull Rancho",
+  "NodeStake",
+  "Mondrago#9340 (@Mr__)",
+  "Apollo",
+  "dreamstaker",
+  "Lis1",
+  "ser#8113, serotonin",
+  " 79anvi ",
+  "ArcLing",
+  "cryptech",
+  "Anatolian Team",
+  "ozzycard",
+  "AlxVoy  ANODE.TEAM",
+  "ITpro",
+  "SINNER",
+  "Biker4b",
+  "sashamaxymchuk",
+  "itrocket",
+  "bulevar",
+  "Nodeist",
+  "laserlincoln#3357",
+  "cardex",
+  "cardchain.acloud.pp.ua",
+  "⭐️ PPNV Service ⭐️",
+  "D-stake",
+  "AnnaSot",
+  "Decloud Nodes Lab",
+  "Validator.run",
+  "@JulyC92894454",
+  "superjax",
+  "Carder",
+  "web34ever",
+  "lesnik | UTSA",
+  "STAVR",
+  "КРИПТОГЕНОФФ",
+  "lesya",
+  "ramuchi.tech",
+  "OranG3cluB",
+  "raymondctc#6331 (@holdnostake)",
+  "AM Solutions ",
+  "078F5410FD08B19E",
+  "KolTigin",
+  "Cardel",
+  "@vadyhodler777",
+  "silent",
+  "ark33",
+  "NOXU",
+  "Moonbridge",
+  "liveevilll",
+  "AlexeyM#5409",
+  "SRG0Z10",
+  "Kolot#5637",
+  "Midora",
+];
+</script>
---
+++ src/views/Team/RoadmapPage.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="article-temp width-temp">
+    <div class="dual--column--flex--wrap">
+      <div class="dual--column--text">
+        <div class="qbox--container">
+          <p class="qbox--text">
+            Q1 2019
+          </p>
+        </div>
+        <h2>1. The Beginning</h2>
+        <div>
+          <BulletList
+            :text="[
+              'Develop the game rules and mechanics',
+              'Community owned collectible card game concepting',
+              'Technology: Cosmos Blockchain + Unity Gameclient',
+            ]"
+            :type="['filled', 'filled', 'filled']"
+          />
+        </div>
+        <p class="text--big">
+          Contribute: You can join our team by contacting us on Telegram or
+          Discord.
+        </p>
+      </div>
+    </div>
+
+    <div class="dual--column--flex--wrap">
+      <div class="dual--column--text">
+        <div class="qbox--container">
+          <p class="qbox--text">
+            Q1 2021
+          </p>
+        </div>
+        <h2>2. The Crowd gets angry</h2>
+        <div>
+          <BulletList
+            :text="[
+              'Create cards with the Card Creator',
+              'Test the alpha game client',
+              'Manage cards on the blockchain (testnet)',
+              'Test buff/nerf mechanics via blockchain',
+            ]"
+            :type="['filled', 'open', 'filled', 'filled']"
+          />
+        </div>
+        <p class="text--big">
+          Contribute: Join the team, design cards or join the alpha tests.
+        </p>
+      </div>
+    </div>
+
+    <div class="dual--column--flex--wrap reverse-column">
+      <div class="dual--column--text">
+        <div class="qbox--container">
+          <p class="qbox--text">
+            Q1 2023
+          </p>
+        </div>
+        <h2>3. The Revolution starts</h2>
+        <div>
+          <BulletList
+            :text="[
+              'Blockchain-ready game client is playable',
+              'Creation of Card Sets/Editions',
+              'Startup tests for the Mainnet',
+              'The alpha set + starter decks are finalized',
+            ]"
+            :type="[]"
+          />
+        </div>
+        <p class="text--big">
+          Contribute: Play the beta or join our team <br>
+          Rewarded: Succesful attempts to break our Game Client or Tokenomics.
+        </p>
+      </div>
+    </div>
+
+    <div class="dual--column--flex--wrap">
+      <div class="dual--column--text">
+        <div class="qbox--container">
+          <p class="qbox--text">
+            Q3 2024
+          </p>
+        </div>
+        <h2>4. The Revolution unfolds</h2>
+        <div>
+          <BulletList
+            :text="[
+              'Mainnet launch',
+              'Airdrop to several groups',
+              'Maybe ICO / IDO / LBP',
+              'The alpha set + starter decks',
+            ]"
+            :type="[]"
+          />
+        </div>
+        <p class="text--big">
+          Contribute: Register for the airdrop and token sale.
+        </p>
+      </div>
+    </div>
+
+    <div>
+      <div class="dual--column--text">
+        <div class="qbox--container">
+          <p class="qbox--text">
+            Q1 2025
+          </p>
+        </div>
+        <h2>5. The Crowd is in Control</h2>
+        <div>
+          <BulletList
+            :text="[
+              'Game client is taken to the next level',
+              'User created tournament system',
+              'Continuous release of new sets via set election',
+            ]"
+            :type="[]"
+          />
+        </div>
+        <p class="text--big">
+          It's on you: Participate via card creation, governance votes and
+          playing. Give us your feedback!
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BulletList from "@/components/elements/BulletList.vue";
+
+export default {
+  name: "RoadmapPage",
+  components: { BulletList },
+  data() {
+    return {};
+  },
+  mounted() {},
+};
+</script>
