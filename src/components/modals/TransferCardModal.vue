195f8fa739e4443035768e1b2e6ed7d25bb0a1c6
Patrick Wieth <patrick.wieth@googlemail.com>
2024-11-27 19:33:24 +0100

    a bit more transfercard modal

--- src/components/modals/TransferCardModal.vue
+++ src/components/modals/TransferCardModal.vue
@@ -1,96 +1,35 @@
 <template>
-  <transition name="modal-fade">
-    <div
-      class="modal__backdrop"
-      style="z-index: 1000;"
-    >
-      <div
-        aria-describedby="modalDescription"
-        aria-labelledby="modalTitle"
-        class="modal"
-        role="dialog"
+  <div>
+    Transfer Card
+
+                    <div
+                      class="w-[27.3rem] ml-[2.1rem] mt-3 bg-zinc-300 bg-opacity-20 shadow-inner"
+                    >
+                      <input
+                        v-model="artistAddress"
+                        class="w-full py-3 px-2 mx-3 bg-transparent text-white text-opacity-100 text-s focus:border-black border-0 border-solid focus:outline-none focus:ring-0 placeholder-white placeholder-opacity-50"
+                        placeholder="Add the receiver's wallet address"
+                        maxLength="41"
+                      >
+                    </div>
+
+      <BaseCCButton
+        :type="Color.RED"
+        @click="edit()"
       >
-        <header
-          id="modalTitle"
-          class="modal__header"
-        >
-          <slot name="header">
-            Transfer card
-          </slot>
-          <button
-            aria-label="Close modal"
-            type="button"
-            class="btn--close"
-            @click="close"
-          >
-            x
-          </button>
-        </header>
-        <div class="modal__body input--transfer">
-          Transfer card {{ card }} to
-          <input
-            v-model="recipient"
-            type="text"
-            placeholder="'recipient'"
-          >
-          <button
-            class="btn--default"
-            @click="send"
-          >
-            Send
-          </button>
-          <a
-            v-show="warningText"
-            class="warning"
-          >
-            <br>{{ warningText }}
-          </a>
-        </div>
-      </div>
-    </div>
-  </transition>
+        Edit card
+      </BaseCCButton>
+  </div>
 </template>
 
-<script lang=ts>
+<script setup lang=ts>
 
 import { validAddress } from "@/utils/validation";
 import { useTx } from "@/def-composables/useTx";
+import { Color } from "@/components/utils/color";
+import BaseCCButton from "@/components/elements/CCButton/BaseCCButton.vue";
 
-export default {
-  name: 'TransferCardModal',
-  props: {
-    card: {
-      type: String,
-      default: ""
-    }
-  },
-  setup() {
-    const { transferCard } = useTx()
-
-    return { transferCard }
-  },
-  data() {
-    return {
-      recipient: "",
-      warningText: "",
-    }
-  },
-  methods: {
-    close() {
-      this.$emit('close')
-    },
-    send() {
-      if (!validAddress(this.recipient)) {
-        this.warningText = "Input a proper address!"
-        return
-      }
-      this.warningText = ""
-      this.transferCard(this.card, this.recipient, _ => {
-        this.close()
-      }, () => {})
-    }
-  }
-}
+const { transferCard } = useTx()
 
 </script>
 
